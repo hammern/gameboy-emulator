8e49cf25977bd579a58e0d819e096efa3e437743
Nadav Hammer <twindata36@gmail.com>
2025-03-21 10:18:33 +0200

    feat: cpu o-x instructions

--- src/cpu.c
+++ src/cpu.c
@@ -588,6 +588,556 @@ static void ld_sp_hl(struct CPU *cpu)
 
 static void nop(struct CPU *cpu) { cpu->cycles += 1; }
 
+static void or_a_r8(struct CPU *cpu, uint8_t reg)
+{
+    cpu->registers.a |= reg;
+
+    cpu->registers.f.z = cpu->registers.a == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0;
+
+    cpu->cycles += 1;
+}
+
+static void or_a_hl(struct CPU *cpu)
+{
+    cpu->registers.a |= read_byte(cpu, cpu->registers.hl);
+
+    cpu->registers.f.z = cpu->registers.a == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0;
+
+    cpu->cycles += 2;
+}
+
+static void or_a_n8(struct CPU *cpu)
+{
+    cpu->registers.a |= read_byte(cpu, cpu->registers.pc);
+    cpu->registers.pc += 1;
+
+    cpu->registers.f.z = cpu->registers.a == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0;
+
+    cpu->cycles += 2;
+}
+
+// TODO
+static void pop_af(struct CPU *cpu)
+{
+    cpu->registers.af = read_word(cpu, cpu->registers.sp);
+    cpu->registers.sp += 2;
+
+    cpu->registers.f.z = 0; // TODO Set from bit 7 of the popped low byte.
+    cpu->registers.f.n = 0; // TODO Set from bit 6 of the popped low byte.
+    cpu->registers.f.h = 0; // TODO Set from bit 5 of the popped low byte.
+    cpu->registers.f.c = 0; // TODO Set from bit 4 of the popped low byte.
+
+    cpu->cycles += 3;
+}
+
+static void pop_r16(struct CPU *cpu, uint16_t *reg)
+{
+    *reg = read_word(cpu, cpu->registers.sp);
+    cpu->registers.sp += 2;
+
+    cpu->cycles += 3;
+}
+
+static void push_af(struct CPU *cpu)
+{
+    write_byte(cpu, cpu->registers.sp, cpu->registers.a);
+    cpu->registers.sp -= 1;
+    write_byte(cpu, cpu->registers.sp,
+               cpu->registers.f.z << 7 | cpu->registers.f.n << 6 |
+                   cpu->registers.f.h << 5 | cpu->registers.f.c << 4);
+    cpu->registers.sp -= 1;
+
+    cpu->cycles += 4;
+}
+
+// TODO
+static void push_r16(struct CPU *cpu, uint16_t reg)
+{
+    // write_word();
+    cpu->registers.sp -= 2;
+
+    cpu->cycles += 4;
+}
+
+// TODO
+static void res_u3_r8(struct CPU *cpu, uint8_t reg)
+{
+    // Set bit u3 in register r8 to 0. Bit 0 is the rightmost one, bit 7 the
+    // leftmost one.
+
+    cpu->cycles += 2;
+}
+
+// TODO
+static void res_u3_hl(struct CPU *cpu)
+{
+    // Set bit u3 in the byte pointed by HL to 0. Bit 0 is the rightmost one,
+    // bit 7 the leftmost one.
+
+    cpu->cycles += 4;
+}
+
+static void ret(struct CPU *cpu)
+{
+    cpu->registers.pc = read_word(cpu, cpu->registers.sp);
+    cpu->registers.sp += 2;
+
+    cpu->cycles += 4;
+}
+
+// TODO
+static void ret_cc(struct CPU *cpu)
+{
+    uint8_t condition;
+    if (condition) {
+        cpu->cycles += 5;
+    } else {
+        cpu->cycles += 2;
+    }
+}
+
+// TODO
+static void reti(struct CPU *cpu)
+{
+    // Return from subroutine and enable interrupts. This is basically
+    // equivalent to executing EI then RET, meaning that IME is set right after
+    // this instruction.
+
+    cpu->cycles += 4;
+}
+
+// TODO
+static void rl_r8(struct CPU *cpu, uint8_t *reg)
+{
+    // Rotate bits in register r8 left, through the carry flag.
+
+    cpu->registers.f.z = reg == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 2;
+}
+
+// TODO
+static void rl_hl(struct CPU *cpu)
+{
+    // Rotate the byte pointed to by HL left, through the carry flag.
+    uint8_t value = read_byte(cpu, cpu->registers.hl);
+
+    cpu->registers.f.z = value == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 4;
+}
+
+// TODO
+static void rla(struct CPU *cpu)
+{
+    // Rotate register A left, through the carry flag.
+
+    cpu->registers.f.z = 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 1;
+}
+
+// TODO
+static void rlc_r8(struct CPU *cpu, uint8_t *reg)
+{
+    // Rotate register r8 left.
+
+    cpu->registers.f.z = *reg == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 2;
+}
+
+// TODO
+static void rlc_hl(struct CPU *cpu)
+{
+    // Rotate register r8 left.
+    uint8_t value = read_byte(cpu, cpu->registers.hl);
+
+    cpu->registers.f.z = value == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 4;
+}
+
+// TODO
+static void rlca(struct CPU *cpu)
+{
+    // Rotate register A left.
+
+    cpu->registers.f.z = 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 1;
+}
+
+// TODO
+static void rr_r8(struct CPU *cpu, uint8_t *reg)
+{
+    // Rotate register r8 right, through the carry flag.
+
+    cpu->registers.f.z = *reg == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 2;
+}
+
+// TODO
+static void rr_hl(struct CPU *cpu)
+{
+    // Rotate the byte pointed to by HL right, through the carry flag.
+    uint8_t value = read_byte(cpu, cpu->registers.hl);
+
+    cpu->registers.f.z = value == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 4;
+}
+
+// TODO
+static void rra(struct CPU *cpu)
+{
+    // Rotate register A right, through the carry flag.
+
+    cpu->registers.f.z = 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 1;
+}
+
+// TODO
+static void rrc_r8(struct CPU *cpu, uint8_t *reg)
+{
+    // Rotate register r8 right.
+
+    cpu->registers.f.z = *reg == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 2;
+}
+
+// TODO
+static void rrc_hl(struct CPU *cpu)
+{
+    // Rotate the byte pointed to by HL right.
+    uint8_t value = read_byte(cpu, cpu->registers.hl);
+
+    cpu->registers.f.z = value == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 4;
+}
+
+// TODO
+static void rrca(struct CPU *cpu)
+{
+    // Rotate register A right.
+
+    cpu->registers.f.z = 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 1;
+}
+
+// TODO
+static void rst_vec(struct CPU *cpu)
+{
+    // Call address vec. This is a shorter and faster equivalent to CALL for
+    // suitable values of vec.
+
+    cpu->cycles += 4;
+}
+
+static void sbc_a_r8(struct CPU *cpu, uint8_t *reg)
+{
+    cpu->registers.a -= (*reg + cpu->registers.f.c);
+
+    cpu->registers.f.z = cpu->registers.a == 0;
+    cpu->registers.f.n = 1;
+    cpu->registers.f.h = 0; // TODO Set if borrow from bit 4.
+    cpu->registers.f.c = 0; // TODO Set if borrow (i.e. if (r8 + carry) > A).
+
+    cpu->cycles += 1;
+}
+
+static void sbc_a_hl(struct CPU *cpu)
+{
+    cpu->registers.a -=
+        (read_byte(cpu, cpu->registers.hl) + cpu->registers.f.c);
+
+    cpu->registers.f.z = cpu->registers.a == 0;
+    cpu->registers.f.n = 1;
+    cpu->registers.f.h = 0; // TODO Set if borrow from bit 4.
+    cpu->registers.f.c = 0; // TODO Set if borrow (i.e. if (r8 + carry) > A).
+
+    cpu->cycles += 2;
+}
+
+static void sbc_a_n8(struct CPU *cpu)
+{
+    cpu->registers.a -=
+        (read_byte(cpu, cpu->registers.pc) + cpu->registers.f.c);
+    cpu->registers.pc += 1;
+
+    cpu->registers.f.z = cpu->registers.a == 0;
+    cpu->registers.f.n = 1;
+    cpu->registers.f.h = 0; // TODO Set if borrow from bit 4.
+    cpu->registers.f.c = 0; // TODO Set if borrow (i.e. if (r8 + carry) > A).
+
+    cpu->cycles += 2;
+}
+
+static void scf(struct CPU *cpu)
+{
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 1;
+
+    cpu->cycles += 1;
+}
+
+// TODO
+static void set_u3_r8(struct CPU *cpu)
+{
+    // Set bit u3 in register r8 to 1. Bit 0 is the rightmost one, bit 7 the
+    // leftmost one.
+
+    cpu->cycles += 2;
+}
+
+// TODO
+static void set_u3_hl(struct CPU *cpu)
+{
+    // Set bit u3 in the byte pointed by HL to 1. Bit 0 is the rightmost one,
+    // bit 7 the leftmost one.
+
+    cpu->cycles += 4;
+}
+
+// TODO
+static void sla_r8(struct CPU *cpu, uint8_t *reg)
+{
+    // Shift Left Arithmetically register r8.
+
+    cpu->registers.f.z = *reg == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 2;
+}
+
+// TODO
+static void sla_hl(struct CPU *cpu)
+{
+    // Shift Left Arithmetically the byte pointed to by HL.
+    uint8_t value = read_byte(cpu, cpu->registers.hl);
+
+    cpu->registers.f.z = value == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 4;
+}
+
+// TODO
+static void sra_r8(struct CPU *cpu, uint8_t *reg)
+{
+    // Shift Right Arithmetically register r8 (bit 7 of r8 is unchanged).
+
+    cpu->registers.f.z = *reg == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 2;
+}
+
+// TODO
+static void sra_hl(struct CPU *cpu)
+{
+    // Shift Right Arithmetically the byte pointed to by HL (bit 7 of the byte
+    // pointed to by HL is unchanged).
+    uint8_t value = read_byte(cpu, cpu->registers.hl);
+
+    cpu->registers.f.z = value == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 4;
+}
+
+// TODO
+static void srl_r8(struct CPU *cpu, uint8_t *reg)
+{
+    // Shift Right Logically register r8.
+
+    cpu->registers.f.z = *reg == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 2;
+}
+
+// TODO
+static void srl_hl(struct CPU *cpu)
+{
+    // Shift Right Logically the byte pointed to by HL.
+    uint8_t value = read_byte(cpu, cpu->registers.hl);
+
+    cpu->registers.f.z = value == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0; // TODO Set according to result.
+
+    cpu->cycles += 4;
+}
+
+// TODO
+static void stop(struct CPU *cpu)
+{
+    // https://gbdev.io/pandocs/Reducing_Power_Consumption.html#using-the-stop-instruction
+}
+
+static void sub_a_r8(struct CPU *cpu, uint8_t reg)
+{
+    cpu->registers.a -= reg;
+
+    cpu->registers.f.z = cpu->registers.a == 0;
+    cpu->registers.f.n = 1;
+    cpu->registers.f.h = 0; // TODO Set if borrow from bit 4.
+    cpu->registers.f.c = 0; // TODO Set if borrow (i.e. if r8 > A).
+
+    cpu->cycles += 1;
+}
+
+static void sub_a_hl(struct CPU *cpu)
+{
+    cpu->registers.a -= read_byte(cpu, cpu->registers.hl);
+
+    cpu->registers.f.z = cpu->registers.a == 0;
+    cpu->registers.f.n = 1;
+    cpu->registers.f.h = 0; // TODO Set if borrow from bit 4.
+    cpu->registers.f.c = 0; // TODO Set if borrow (i.e. if r8 > A).
+
+    cpu->cycles += 2;
+}
+
+static void sub_a_n8(struct CPU *cpu)
+{
+    cpu->registers.a -= read_byte(cpu, cpu->registers.pc);
+    cpu->registers.pc += 1;
+
+    cpu->registers.f.z = cpu->registers.a == 0;
+    cpu->registers.f.n = 1;
+    cpu->registers.f.h = 0; // TODO Set if borrow from bit 4.
+    cpu->registers.f.c = 0; // TODO Set if borrow (i.e. if r8 > A).
+
+    cpu->cycles += 2;
+}
+
+static void swap_r8(struct CPU *cpu, uint8_t *reg)
+{
+    *reg = (*reg << 4) | (*reg >> 4);
+
+    cpu->registers.f.z = *reg == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0;
+
+    cpu->cycles += 2;
+}
+
+static void swap_hl(struct CPU *cpu)
+{
+    uint8_t value = read_byte(cpu, cpu->registers.hl);
+    value = (value << 4) | (value >> 4);
+    write_byte(cpu, cpu->registers.hl, value);
+
+    cpu->registers.f.z = value == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0;
+
+    cpu->cycles += 4;
+}
+
+static void xor_a_r8(struct CPU *cpu, uint8_t reg)
+{
+    cpu->registers.a ^= reg;
+
+    cpu->registers.f.z = cpu->registers.a == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0;
+
+    cpu->cycles += 1;
+}
+
+static void xor_a_hl(struct CPU *cpu)
+{
+    cpu->registers.a ^= read_byte(cpu, cpu->registers.hl);
+
+    cpu->registers.f.z = cpu->registers.a == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0;
+
+    cpu->cycles += 2;
+}
+
+static void xor_a_n8(struct CPU *cpu)
+{
+    cpu->registers.a ^= read_byte(cpu, cpu->registers.pc);
+    cpu->registers.pc += 1;
+
+    cpu->registers.f.z = cpu->registers.a == 0;
+    cpu->registers.f.n = 0;
+    cpu->registers.f.h = 0;
+    cpu->registers.f.c = 0;
+
+    cpu->cycles += 2;
+}
+
 void cpu_execute(struct CPU *cpu)
 {
     if (!cpu->cycles) {
